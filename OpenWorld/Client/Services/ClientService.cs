98533b13200b1d15ced30724ddf495fe124be379
김주영 <robolab1902@ringgames.com>
2022-11-10 19:02:05 +0900

    Server, Client 살짝 수정

--- OpenWorld/Client/Services/ClientService.cs
+++ OpenWorld/Client/Services/ClientService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +12,27 @@ namespace Client.Services
     {
         public void Start()
         {
+            Console.WriteLine("I'm Client!");
+
+            TcpClient client = new TcpClient();
+
+            client.Connect("127.0.0.1", 9483);
+
+            byte[] buffer = Encoding.Default.GetBytes("Hi! Server! I'm Client!");
+
+            int i = 0;
             while (true)
             {
-                Console.WriteLine("I'm Client!");
+                if(i > 5)
+                {
+                    break;
+                }
+                i++;
+                client.GetStream().Write(buffer, 0, buffer.Length);    
                 Task.Delay(1000).Wait();
             }
+
+            client.Close();
         }
     }
 }
