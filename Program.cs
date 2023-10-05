e61afff5bd1e45b96db98d97a5e33b03f15ad80a
JuYoung Kim <robolab1902@naver.com>
2023-10-05 12:22:19 +0900

    MongoDB 가 이러는 것만으로 DB 생성이 되고 데이터가 저장이 된다.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+
+string connString = "mongodb://localhost";
+
+MongoClient cli = new MongoClient(connString);
+
+// testdb 라는 데이터 베이스 가져오기
+var testdb = cli.GetDatabase("testdb");
+
+var customers = testdb.GetCollection<Customer>("Customers");
+
+Customer cust1 = new Customer { Name = "Kim", Age = 30 };
+customers.InsertOne(cust1);
+ObjectId id = cust1.Id;
+
+// Select - ID 로 검색
+var result = customers.Find( x => x.Id == id).FirstOrDefault();
+
+if(result is not null)
+{
+    Console.WriteLine(result.ToString());
+}
+
+class Customer
+{
+    public ObjectId Id { get; set; }
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return Name + " " + Age;
+    }
+}
